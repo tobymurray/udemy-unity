2ba3aaa5b214948a7eab57e6573ed0d62d1959bf
Toby Murray <murray.toby@gmail.com>
2015-10-25 14:19:40 -0400

    Lecture 15

--- NumberWizard/Assets/scripts/NumberWizard.cs
+++ NumberWizard/Assets/scripts/NumberWizard.cs
@@ -5,7 +5,8 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me!");
 	}
 	
 	// Update is called once per frame
