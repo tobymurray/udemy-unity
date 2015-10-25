2e179962f357e82a5228590aee7c632b54ebd9a3
Toby Murray <murray.toby@gmail.com>
2015-10-25 14:22:33 -0400

    Lecture 16

--- NumberWizard/Assets/scripts/NumberWizard.cs
+++ NumberWizard/Assets/scripts/NumberWizard.cs
@@ -7,6 +7,12 @@ public class NumberWizard : MonoBehaviour {
 	void Start () {
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
+
+		int min = 1;
+		int max = 1000;
+
+		print ("The lowest number you can pick is " + min);
+		print ("The highest number you can pick is " + max);
 	}
 	
 	// Update is called once per frame
