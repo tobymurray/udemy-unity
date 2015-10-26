5e6a693b20e08d9a6b500579c0cd665c52d20345
Toby Murray <murray.toby@gmail.com>
2015-10-26 02:00:24 -0400

    Lecture 29

---
+++ Text101/Assets/TextController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text m_text;
+
+	// Use this for initialization
+	void Start () {
+		m_text.text = "Hello World";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			m_text.text = "Space key pressed";
+		}
+	}
+}
