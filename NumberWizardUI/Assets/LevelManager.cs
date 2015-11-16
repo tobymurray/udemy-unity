4627dbb5e4457cf7428f4f8bf0b5e878cd8426f6
Toby Murray <murray.toby@gmail.com>
2015-11-15 20:37:24 -0500

    Lecture 46

---
+++ NumberWizardUI/Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for: " + name);
+	}
+
+	public void QuitRequested() {
+		Debug.Log ("I want to quit!");
+	}
+
+}
