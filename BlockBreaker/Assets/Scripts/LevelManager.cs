8f44666ff3d499cbfd4893f6f07674114fd79fba
Toby Murray <murray.toby@gmail.com>
2015-11-29 23:38:35 -0500

    Lecture 61: Import the Menu System

---
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequested() {
+		Debug.Log ("I want to quit!");
+		Application.Quit ();
+	}
+
+}
