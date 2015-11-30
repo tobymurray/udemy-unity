9675a05cd212c49e63d63f9accd9b1ee5cdd952d
Toby Murray <murray.toby@gmail.com>
2015-11-30 00:14:15 -0500

    Lecture 64: Using Static Variables

--- BlockBreaker/Assets/Scripts/MusicPlayer.cs
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -3,9 +3,18 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
+	private static MusicPlayer s_instance;
+
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad (gameObject);
+		if (s_instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			s_instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
+
 	}
 	
 	// Update is called once per frame
