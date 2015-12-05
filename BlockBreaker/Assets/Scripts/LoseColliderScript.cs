97e06c1c83aa91d5791fef327b82b3cd645c7142
Toby Murray <murray.toby@gmail.com>
2015-12-04 20:13:15 -0500

    Lecture 69: Colliders, Triggers, & Collisions

---
+++ BlockBreaker/Assets/Scripts/LoseColliderScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseColliderScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D trigger) {
+		print ("Trigger");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision2D) {
+		print ("Collision");
+	}
+}
