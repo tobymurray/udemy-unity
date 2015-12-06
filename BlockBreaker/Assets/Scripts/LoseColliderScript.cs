2198e1761a718fc1e692b903f91a60c88c57c01f
Toby Murray <murray.toby@gmail.com>
2015-12-05 20:32:37 -0500

    Lecture 70: Tidying up before moving on

--- BlockBreaker/Assets/Scripts/LoseColliderScript.cs
+++ BlockBreaker/Assets/Scripts/LoseColliderScript.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class LoseColliderScript : MonoBehaviour {
 
+	public LevelManager m_levelManager;
+
 	void OnTriggerEnter2D(Collider2D trigger) {
 		print ("Trigger");
 	}
 
 	void OnCollisionEnter2D(Collision2D collision2D) {
 		print ("Collision");
+		m_levelManager.LoadLevel ("Win");
 	}
 }
