928ce120c4f68724e306f826bc065aa07e103311
Toby Murray <murray.toby@gmail.com>
2015-12-05 21:28:53 -0500

    Lecture 74: Launching Ball on Mouse Click

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+
+	public Paddle m_paddle;
+	private Vector3 m_vectorToBall;
+	private bool m_hasStarted = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_vectorToBall = this.transform.position - m_paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!m_hasStarted) {
+			// Lock the ball relative to the paddle
+			this.transform.position = m_paddle.transform.position + m_vectorToBall;
+
+			if (Input.GetMouseButtonDown (0)) {
+				print ("Mouse clicked");
+				this.rigidbody2D.velocity = new Vector2 (2f, 10f);
+				m_hasStarted = true;
+			}
+		}
+	}
+}
