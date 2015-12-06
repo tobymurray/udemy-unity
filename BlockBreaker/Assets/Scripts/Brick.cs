7403928e2a1ecd9d87e04638b1f84b911a8506ea
Toby Murray <murray.toby@gmail.com>
2015-12-05 21:55:21 -0500

    Lecture 76: Creating & Using Unity Prefabs

---
+++ BlockBreaker/Assets/Scripts/Brick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brick : MonoBehaviour {
+
+	public int m_maxHits;
+	private int m_timesHit;
+
+	// Use this for initialization
+	void Start () {
+		m_timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision2D) {
+		m_timesHit++;
+		print (m_timesHit);
+	}
+}
