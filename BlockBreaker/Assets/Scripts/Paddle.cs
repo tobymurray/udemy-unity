e61ca5f0686422e32c63d96c5c562a6d6627588b
Toby Murray <murray.toby@gmail.com>
2015-12-05 21:13:20 -0500

    Lecture 73: Movement by mouse

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseXInBlocks = Input.mousePosition.x / Screen.width * 16;
+
+		this.transform.position = new Vector3 (Mathf.Clamp(mouseXInBlocks, 0.5f, 15.5f), this.transform.position.y);
+	}
+}
