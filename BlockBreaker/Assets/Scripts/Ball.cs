34c81d84bd98e4e18fe07f082d7ed04680fb4575
Toby Murray <murray.toby@gmail.com>
2016-09-03 23:17:59 -0400

    Lecture 89: Using Random.Range()

--- BlockBreaker/Assets/Scripts/Ball.cs
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -31,8 +31,11 @@ public class Ball : MonoBehaviour
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+
 		if (m_hasStarted) {
 			audio.Play ();
+			rigidbody2D.velocity += tweak;
 		}
 	}
 }
