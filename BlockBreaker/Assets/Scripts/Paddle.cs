c1bba2d44a6cc45f9cfefe93d50fe1ba078f538e
Toby Murray <murray.toby@gmail.com>
2016-09-03 23:32:11 -0400

    Lecture 90: Automated Play Testing

--- BlockBreaker/Assets/Scripts/Paddle.cs
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -3,15 +3,32 @@ using System.Collections;
 
 public class Paddle : MonoBehaviour {
 
+	public bool m_autoPlay = false;
+	private Ball m_ball;
+
 	// Use this for initialization
 	void Start () {
-	
+		m_ball = GameObject.FindObjectOfType<Ball> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float mouseXInBlocks = Input.mousePosition.x / Screen.width * 16;
+		if (!m_autoPlay) {
+			MoveWithMouse ();
+		} else {
+			AutoPlay();
+		}
+	}
 
+	void MoveWithMouse() {
+		float mouseXInBlocks = Input.mousePosition.x / Screen.width * 16;
 		this.transform.position = new Vector3 (Mathf.Clamp(mouseXInBlocks, 0.5f, 15.5f), this.transform.position.y);
+	}
+
+	void AutoPlay() {
+		Vector3 ballPosition = m_ball.transform.position;
+		Vector3 paddlePosition = new Vector3 (Mathf.Clamp (ballPosition.x, 0.5f, 15.5f), this.transform.position.y, 0f);
+
+		this.transform.position = paddlePosition;
 	}
 }
