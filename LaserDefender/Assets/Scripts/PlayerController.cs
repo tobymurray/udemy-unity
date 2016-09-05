ecffc9b2d2ba10d6f633e62249f801b44353ace4
Toby Murray <murray.toby@gmail.com>
2016-09-05 18:36:30 -0400

    Lecture 104: A Starship We Can Control

---
+++ LaserDefender/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float m_speed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow) && !Input.GetKey (KeyCode.RightArrow)) {
+			transform.position += new Vector3 (-m_speed * Time.deltaTime, 0, 0);
+		} else if (Input.GetKey (KeyCode.RightArrow) && !Input.GetKey (KeyCode.LeftArrow)) {
+			transform.position += new Vector3 (m_speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
