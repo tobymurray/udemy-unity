a9bf6f8e61793330c288f1bdd848b66cf181d465
Toby Murray <murray.toby@gmail.com>
2016-09-06 00:36:36 -0400

    Lecture 110: Shooting Enemies

---
+++ LaserDefender/Assets/Entities/Enemy/EnemyBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	public float m_health;
+	
+	void OnTriggerEnter2D(Collider2D collider) {
+		Projectile projectile = collider.gameObject.GetComponent<Projectile> ();
+		if (projectile) {
+			m_health -= projectile.GetDamage();
+			projectile.Hit ();
+
+			if (m_health <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
---
+++ LaserDefender/Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+	
+	public float m_damage = 100f;
+	
+	public void Hit ()
+	{
+		Destroy (gameObject);
+	}
+
+	public float GetDamage ()
+	{
+		return m_damage;
+	}
+}
