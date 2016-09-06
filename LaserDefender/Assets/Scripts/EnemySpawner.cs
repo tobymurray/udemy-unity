f3ed14cf85bb4f4fbf293e27abe543fbaa9c4230
Toby Murray <murray.toby@gmail.com>
2016-09-05 22:33:56 -0400

    Lecture 106: Creating the Enemies

---
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject m_enemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		GameObject enemy = Instantiate (m_enemyPrefab, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
+		enemy.transform.parent = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
