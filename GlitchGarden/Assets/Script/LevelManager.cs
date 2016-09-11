73cfbf5ba684c7e3f62659186ddcdd2666e791b6
Toby Murray <murray.toby@gmail.com>
2016-09-10 23:50:21 -0400

    Lecture 129: Making A Splash Screen

---
+++ GlitchGarden/Assets/Script/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+	public float m_autoLoadNextLevelAfter;
+
+	void Start ()
+	{
+		Invoke ("LoadNextLevel", m_autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel (string name)
+	{
+		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest ()
+	{
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel ()
+	{
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+}
