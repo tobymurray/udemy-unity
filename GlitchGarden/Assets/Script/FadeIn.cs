ba5c48c0eb591543c650b2d5862852752664849b
Toby Murray <murray.toby@gmail.com>
2016-10-02 17:56:32 -0400

    Lecture 133: Adding Fade Transitions

---
+++ GlitchGarden/Assets/Script/FadeIn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeIn : MonoBehaviour
+{
+
+	public float m_fadeInTime;
+	private Image m_fadePanel;
+	private Color m_currentColor = Color.black;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_fadePanel = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Time.timeSinceLevelLoad < m_fadeInTime) {
+			float alphaChange = Time.deltaTime / m_fadeInTime;
+			m_currentColor.a -= alphaChange;
+			m_fadePanel.color = m_currentColor;
+		} else {
+			gameObject.SetActive (false);
+		}
+	}
+}
